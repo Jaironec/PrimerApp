eacf083124181320a6f75084c5449f184f938d12
operador <16cardenas16@gmail.com>
2022-02-07 13:01:52 -0500

    Final app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Hola from "./components/Hola";
+//import Hola from "./components/Hola";
+import Contador from "./components/Contador";
+
 
 
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo REACT</h1> 
-      <Hola mensaje="Este es un mensaje" pie="La fecha es 2022-feb-07"/>
+{     <h1>Hola mundo REACT</h1> 
+/*       <Hola mensaje="Este es un mensaje" pie="La fecha es 2022-feb-07"/>
       <Hola mensaje="Esmeraldas" pie="Es una ciudad hermosa"/>
-      <Hola mensaje="Ecuador" pie="País Multicultural"/>
-
+      <Hola mensaje="Ecuador" pie="País Multicultural"/> */} 
+<Contador/>
     </div>
   );
 }
