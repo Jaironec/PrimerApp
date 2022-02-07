33c82dda21144534d5b34b92d6fd939f4cb18335
operador <16cardenas16@gmail.com>
2022-02-07 12:29:30 -0500

    Primer app

---
+++ src/components/Hola.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+ function Hola() {
+  return (
+    <div>
+      <hl>PUCESE</hl>
+      <h2>{}</h2>
+      <h3>Mi primer componente react!!!</h3>
+    </div>
+  );
+}
+
+export default Hola;
